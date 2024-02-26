46560516c8d132d4f2bd9f3ca211a42a17d7af67
Evgenii Kotenev <koteniov.zgenya@mail.ru>
2024-02-26 11:59:46 +0300

    fix bugs

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import 'normalize.css'
-import './index.css'
-import './fonts.css'
+import '.index.css'
+import '.fonts.css'
 import {HashRouter} from 'react-router-dom';
 import {store} from './redux/store'
 import {Provider} from 'react-redux';
