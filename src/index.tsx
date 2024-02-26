5eeea68b27baf5bcfa8dbacfe6952a4b4be75571
Evgenii Landau <120317060+evgenii-landau@users.noreply.github.com>
2024-02-26 08:58:41 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ import {Provider} from 'react-redux';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename='react-pizza'>
 			<Provider store={store}>
 				<App/>
 			</Provider>
